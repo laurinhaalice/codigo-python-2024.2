18b19730088a3306e5989671b81943ce9065ea12
laura alice <lagglaura@gmail.com>
2024-10-02 19:35:09 -0300

    exemplo 1 atualização
    melhorei o mapa do tesouro

--- aula 2/exemplo1.py
+++ aula 2/exemplo1.py
@@ -1,5 +1,6 @@
 #esses serao passos do pirata
+print("este é o caminho para o mapa do tesouro")
 print("Andar para esquerda")
 print("Andar para direita")
 print("Andar para baixo")
-
+print("procurar o X e cavar")
