9816e2cd092a8252b383d57fe9a69f5717311e9d
laura alice <lagglaura@gmail.com>
2024-10-02 20:56:13 -0300

    calculo do IPVA de um carro
    calculei o valor de um carro multiplicando a porcentagem do IPVA em decimal pelo valor do carro

---
+++ aula 3/exercício1.py
@@ -0,0 +1,5 @@
+#calcular o valor do IPVA de um carro
+valordocarro = float(input("qual o valor do seu carro?"))
+IPVA =0.04
+print(valordocarro*IPVA)
+print(f"o valor que você terá que pagar do imposto é: {valordocarro*IPVA}")
