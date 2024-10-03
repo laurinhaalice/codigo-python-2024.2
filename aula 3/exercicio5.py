58d54d9e37652af3bdbc90c4442544eb49e6b982
laura alice <lagglaura@gmail.com>
2024-10-02 21:32:49 -0300

    calculo da coleira e o dobro do sapato e o triplo de uma fuciinheira para um cachorro
    calculo da coleira, sapato e fucinheira

---
+++ aula 3/exercicio5.py
@@ -0,0 +1,6 @@
+coleiradocachorro = int(input("Qual o tamanho da coleira do seu cachorro?"))
+sapatodocachorro =(coleiradocachorro*2)
+print(f"o tamanho do sapato é: {sapatodocachorro}")
+#Vamos calcular o tomanho da fucinheira agora
+fucinheiradocachorro =(sapatodocachorro*3)
+print(f"o tamanha da fucinheira é:{fucinheiradocachorro}")
