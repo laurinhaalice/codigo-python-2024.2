51485bb5d76ee5e4ea8921d57a45bc34a4fde00e
laura alice <lagglaura@gmail.com>
2024-10-02 21:20:34 -0300

    calculo do IMC
    Calculei o IMC

--- aula 3/exercicio2.py
+++ aula 3/exercicio2.py
@@ -1,4 +1,4 @@
-altura = int(input("Qual a sua altura em metros?"))
-peso = int(input("Qual o seu peso em quilos?"))
-IMC = peso/altura*altura
+altura = float(input("Qual a sua altura em metros?"))
+peso = float(input("Qual o seu peso em quilos?"))
+IMC = (peso/(altura*altura))
 print(f"A sua índice de massa corporal é:{IMC}")
